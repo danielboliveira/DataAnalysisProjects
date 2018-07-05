f0e57d56e11b36d12d2d120d19b23caa830a5a1d
Daniel Oliveira <daniel@danielboliveira.com>
2018-07-05 20:05:55 -0300

    b

--- TwitterAnalyzer/Automacao/Stats.py
+++ TwitterAnalyzer/Automacao/Stats.py
@@ -44,6 +44,21 @@ def generateStatsSentimentoGraphs(consulta_id):
         fig = plt.get_figure()
         fig.savefig(file_graph_2) 
         
+        
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        
+    
+def generateStatsSentimentoPieGraph(consulta_id): 
+    try:    
+        df = an.getStatsTwitters(consulta_id)
+        
+        path = utils.getAnalisesPath(consulta_id)
+        
+        file_graph_1 = path+"\\"+"sentimentos_pie.png"
+        utils.removeFile(file_graph_1)
+        
+        
         ds = df.describe()
         r = ds.drop(['std','count','min','max','25%','50%','75%']).drop(['Negativos','Neutros','Positivos'],axis=1)
         
@@ -58,15 +73,10 @@ def generateStatsSentimentoGraphs(consulta_id):
         explode = (0.1, 0, 0, 0)  # explode 1st slice
          
         # Plot
-        file_graph_1 = path+"\\"+"sentimentos_media.png"
-        utils.removeFile(file_graph_1)
         plt_pie.pie(sizes, labels=labels, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
         plt_pie.axis('equal')
-        fig = plt_pie.figure()
-        fig.savefig(file_graph_1)        
+        plt_pie.show()
+        plt_pie.savefig(file_graph_1)        
         
     except Exception as e:
         logging.error(traceback.format_exc())
-        
-    
-
