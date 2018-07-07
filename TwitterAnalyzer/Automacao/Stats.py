cb9fa73ed744ac1d0127c16f3abc8a7228d80544
Daniel Oliveira <daniel@danielboliveira.com>
2018-07-07 08:10:22 -0300

    a

--- TwitterAnalyzer/Automacao/Stats.py
+++ TwitterAnalyzer/Automacao/Stats.py
@@ -13,19 +13,24 @@ import matplotlib.pyplot as plt_pie
 
 def generateStatsSentimentoGraphs(consulta_id): 
     try:    
+        termo = an.getTermo(consulta_id)
+        
+        if (not termo):
+            return
+        
         df = an.getStatsTwitters(consulta_id)
         
         path = utils.getAnalisesPath(consulta_id)
         
         file_graph_1 = path+"\\"+"sentimentos_line.png"
         utils.removeFile(file_graph_1)
-        plt = df.plot(figsize=(20,5),kind='line',x=['Horario'],y=['%Positivos','%Neutros','%Negativos'],color=['b', 'lightgray', 'r'],title = 'Variação de sentimento')        
+        plt = df.plot(figsize=(20,5),kind='line',x=['Horario'],y=['%Positivos','%Neutros','%Negativos'],color=['b', 'lightgray', 'r'],title = 'Variação de sentimento({0})'.format(termo.upper()))        
         fig = plt.get_figure()
         fig.savefig(file_graph_1)        
         
         file_graph_2 = path+"\\"+"sentimentos_bar.png"
         utils.removeFile(file_graph_2)
-        plt = df.plot(figsize=(10,4),kind='bar',x=['Horario'],y=['%Positivos','%Neutros','%Negativos'],stacked=True,color=['b', 'lightgray', 'r'],title = 'Variação de sentimento')    
+        plt = df.plot(figsize=(10,4),kind='bar',x=['Horario'],y=['%Positivos','%Neutros','%Negativos'],stacked=True,color=['b', 'lightgray', 'r'],title = 'Variação de sentimento({0})'.format(termo.upper()))    
         fig = plt.get_figure()
         fig.savefig(file_graph_2)        
         
@@ -34,13 +39,13 @@ def generateStatsSentimentoGraphs(consulta_id):
         df = an.getStatsTwitters(consulta_id,True)
         file_graph_1 = path+"\\"+"sentimentos_line_influencia.png"
         utils.removeFile(file_graph_1)
-        plt = df.plot(figsize=(20,5),kind='line',x=['Horario'],y=['%Positivos','%Neutros','%Negativos'],color=['b', 'lightgray', 'r'],title = 'Variação de sentimento')        
+        plt = df.plot(figsize=(20,5),kind='line',x=['Horario'],y=['%Positivos','%Neutros','%Negativos'],color=['b', 'lightgray', 'r'],title = 'Variação de sentimento({0})'.format(termo.upper()))        
         fig = plt.get_figure()
         fig.savefig(file_graph_1)        
         
         file_graph_2 = path+"\\"+"sentimentos_bar_influencia.png"
         utils.removeFile(file_graph_2)
-        plt = df.plot(figsize=(10,4),kind='bar',x=['Horario'],y=['%Positivos','%Neutros','%Negativos'],stacked=True,color=['b', 'lightgray', 'r'],title = 'Variação de sentimento')    
+        plt = df.plot(figsize=(10,4),kind='bar',x=['Horario'],y=['%Positivos','%Neutros','%Negativos'],stacked=True,color=['b', 'lightgray', 'r'],title = 'Variação de sentimento({0})'.format(termo.upper()))    
         fig = plt.get_figure()
         fig.savefig(file_graph_2) 
         
@@ -70,7 +75,7 @@ def generateStatsSentimentoPieGraph(consulta_id):
         labels = 'Negativos', 'Positivos', 'Neutros'
         sizes = [prcNeg, prcPos, prcNeu]
         colors = ['red', 'blue', 'silver']
-        explode = (0.1, 0, 0, 0)  # explode 1st slice
+#        explode = (0.1, 0, 0, 0)  # explode 1st slice
          
         # Plot
         plt_pie.pie(sizes, labels=labels, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
